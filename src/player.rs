cbec42d109a6f7695d8d2447b741784cbf88ab8c
Cr4zi <ori.mizrahi1000@gmail.com>
2021-09-25 13:03:16 +0300

    Create player struct and added the data for player

---
+++ src/player.rs
@@ -0,0 +1,12 @@
+extern crate reqwest;
+
+struct Player {
+    name: String,
+    id: u8,
+    ping: u16,
+    xbl_id: String,
+    steam_id: String,
+    discord_id: String,
+    live_id: String,
+    license_id: String
+}
