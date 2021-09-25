bd646a68081e29d8aa8645ba825b20f4be25c99b
Cr4zi <ori.mizrahi1000@gmail.com>
2021-09-25 14:19:47 +0300

    Change the structs of server and players to take reference of something and public function for parsing server data

--- src/player.rs
+++ src/player.rs
@@ -3,14 +3,14 @@ extern crate serde_json;
 use serde_json::Value as JsonValue;
 
 struct Player {
-    name: String,
-    id: u8,
-    ping: u16,
-    xbl_id: String,
-    steam_id: String,
-    discord_id: String,
-    live_id: String,
-    license_id: String
+    name: &String,
+    id: &u8,
+    ping: &u16,
+    xbl_id: &String,
+    steam_id: &String,
+    discord_id: &String,
+    live_id: &String,
+    license_id: &String
 }
 
 impl ToString for Player {
---
+++ src/server.rs
@@ -0,0 +1,32 @@
+extern crate serde_json;
+
+use serde_json::Value as JsonValue;
+
+struct Server {
+    hostname: &String,
+    clients: &u8,
+    max_clients: &u8,
+    map_name: &String,
+    game_type: &String
+}
+
+impl ToString for Server {
+    fn to_string(&self) -> String {
+        return format!("<hostname: {}, client: {}>", self.hostname, self.clients)
+    }
+}
+
+pub fn parse_server_data(data: JsonValue) -> Server {
+    let hostname = data["hostname"];
+    let clients = data["clients"];
+    let max_clients = data["sv_maxclients"];
+    let game_type = data["gametype"];
+    let map_name = data["mapname"];
+    return Server {
+        hostname: &hostname,
+        clients: &clients,
+        max_clients: &max_clients,
+        map_name: &map_name,
+        game_type: &game_type
+    }
+}
